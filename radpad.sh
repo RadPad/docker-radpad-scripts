226deb667fbf7b8a273e4fab2b549082024e221d
Michael Clifford <cliffom@gmail.com>
2016-03-15 14:34:16 -0700

    :penguin: Only print id of first instance

--- radpad.sh
+++ radpad.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-alias rpexec="docker exec -it `docker ps | grep radpad/ | awk '{ print $1 }'`"
+alias rpexec="docker exec -it `docker ps | grep radpad/ | awk 'NR==1{ print $1 }'`"
 alias rpbash="rpexec bash -l"
 alias rprun="rpexec bash -lc"
 alias rpmigrate='rprun "bundle exec rake db:migrate"'
