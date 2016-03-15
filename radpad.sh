aea23c2dfb135ffce6b32c7e60fed03643adaf9a
Michael Clifford <cliffom@gmail.com>
2016-03-15 14:45:20 -0700

    :penguin: Comment to test build triggers

--- radpad.sh
+++ radpad.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# A list of helper aliases to work with Docker and Rails
 alias rpexec="docker exec -it `docker ps | grep radpad/ | awk 'NR==1{ print $1 }'`"
 alias rpbash="rpexec bash -l"
 alias rprun="rpexec bash -lc"
