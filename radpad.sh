d6f4ffff0ea7af3d32b0f6c66c922b9800f90b62
Michael Clifford <cliffom@gmail.com>
2016-03-15 14:58:03 -0700

    :penguin: New comment to test trigger

--- radpad.sh
+++ radpad.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # A list of helper aliases to work with Docker and Rails
+
+# There may be multiple containers that match the pattern
+# so only query for the first match
 alias rpexec="docker exec -it `docker ps | grep radpad/ | awk 'NR==1{ print $1 }'`"
 alias rpbash="rpexec bash -l"
 alias rprun="rpexec bash -lc"
