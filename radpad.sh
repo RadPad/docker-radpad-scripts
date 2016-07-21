7b90244ce3d175afea083c0eb49c39d4526135a3
James Welsh <jwelshiv@gmail.com>
2016-07-20 17:48:47 -0700

    :pizza: adds rplogs command to tail app logs

--- radpad.sh
+++ radpad.sh
@@ -6,6 +6,7 @@
 # so only query for the first match
 alias rpexec="docker exec -it `docker ps | grep radpad/ | awk 'NR==1{ print $1 }'`"
 alias rpexec_notty="docker exec `docker ps | grep radpad/ | awk 'NR==1{ print $1 }'`"
+alias rplogs="docker logs -f `docker ps | grep radpad/ | awk 'NR==1{ print $1 }'`"
 alias rpbash="rpexec bash -l"
 alias rprun="rpexec bash -lc"
 alias rpmigrate='rprun "bundle exec rake db:migrate"'
