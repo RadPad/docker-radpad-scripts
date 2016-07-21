fbd990ca5d306f1aae4014390c791115a6bc3097
James Welsh <jwelshiv@gmail.com>
2016-07-21 11:45:02 -0700

    :pizza: export and reuse $DOCKER_CONTAINER

--- radpad.sh
+++ radpad.sh
@@ -4,9 +4,11 @@
 
 # There may be multiple containers that match the pattern
 # so only query for the first match
-alias rpexec="docker exec -it `docker ps | grep radpad/ | awk 'NR==1{ print $1 }'`"
-alias rpexec_notty="docker exec `docker ps | grep radpad/ | awk 'NR==1{ print $1 }'`"
-alias rplogs="docker logs -f `docker ps | grep radpad/ | awk 'NR==1{ print $1 }'`"
+export DOCKER_CONTAINER="`docker ps | grep radpad/ | awk 'NR==1{ print $1 }'`"
+
+alias rpexec="docker exec -it $DOCKER_CONTAINER"
+alias rpexec_notty="docker exec $DOCKER_CONTAINER"
+alias rplogs="docker logs -f $DOCKER_CONTAINER"
 alias rpbash="rpexec bash -l"
 alias rprun="rpexec bash -lc"
 alias rpmigrate='rprun "bundle exec rake db:migrate"'
